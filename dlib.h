fb3763053017e999f5beb4e7896b68ed5a05d46c
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-03-04 17:18:37 -0500

    added documentation and minor fix up

--- dlib.h
+++ dlib.h
@@ -1,3 +1,11 @@
+/**
+ * @file dlib.h
+ * @author Brandon Kirincich
+ * @brief see README 
+ * 
+ * @copyright MIT License
+ * 
+ */
 #if defined(unix) || defined(__unix__) || defined(__unix)
 #define OS_LINUX
 #include <dlfcn.h>
@@ -28,6 +36,12 @@ class dlib {
 
     public:
 
+        /**
+         * @brief Construct a new dlib object and
+         * and load the lib specified by path
+         * 
+         * @param path relative or absolute path to a dynamic lib
+         */
         dlib(const TCHAR *path):
             m_path(path), m_handle()
         {
@@ -36,8 +50,13 @@ class dlib {
 
         ~dlib() = default;
 
-        dlib_handle load_lib(const TCHAR * path) {
-            m_path = path;
+        /**
+         * @brief should only be used internally for the C++ version
+         * 
+         * @param path relative or absolute path to a dynamic lib
+         * @return dlib_handle handle to the dynamic lib
+         */
+        dlib_handle load_lib() {
             #if defined(OS_LINUX)
             return dlopen(m_path, RTLD_NOW);
             #elif defined(OS_WINDOWS)
@@ -47,6 +66,13 @@ class dlib {
             #endif
         }
 
+        /**
+         * @brief return a function pointer 
+         * to the function specified by func_name
+         * 
+         * @param func_name name of function to get from dynamic lib
+         * @return void* function pointer to the function
+         */
         void *get_func(const char *func_name) {
             if (m_handle == nullptr) return nullptr;
             #if defined(OS_LINUX)
@@ -58,6 +84,10 @@ class dlib {
             #endif
         }
 
+        /**
+         * @brief closes the dynamic lib
+         * 
+         */
         void close_lib() {
             #if defined(OS_LINUX)
             dlclose(m_handle);
@@ -70,7 +100,12 @@ class dlib {
 };
 
 #else
-
+/**
+ * @brief load the lib specified by path and return for ti
+ * 
+ * @param path path to a dynamic lib
+ * @return dlib_handle handle to the dynamic lib
+ */
 dlib_handle load_lib(const TCHAR *path) {
     #if defined(OS_LINUX)
     return dlopen(path, RTLD_NOW);
@@ -81,6 +116,14 @@ dlib_handle load_lib(const TCHAR *path) {
     #endif
 }
 
+/**
+ * @brief return a function pointer to the 
+ * function specified by func_name
+ * 
+ * @param handle handle for a dynamic lib
+ * @param func_name name of the funcion to get
+ * @return void* function pointer to the function
+ */
 void *get_func(dlib_handle handle, const char *func_name) {
     if (handle == nullptr) return nullptr;
     #if defined(OS_LINUX)
@@ -92,6 +135,11 @@ void *get_func(dlib_handle handle, const char *func_name) {
     #endif
 }
 
+/**
+ * @brief close the dynamic lib
+ * 
+ * @param handle handle for the dynamic lib
+ */
 void close_lib(dlib_handle handle) {
     #if defined(OS_LINUX)
     dlclose(handle);
